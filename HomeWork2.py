1fd86107d5d9734bffdee92d6acd3d794d16babf
Sasha <krofos@mail.ru>
2019-02-02 17:45:03 +0200

    Добавлено задание 3 и 4

--- HomeWork2.py
+++ HomeWork2.py
@@ -20,3 +20,17 @@ dictionary = {1:'1',
 
 for key, value in dictionary.items():
     print (key, value)
+
+
+# Создать tuple из 10 любых дробных чисел, найти максимальное и минимальное значение в нем
+
+t = (1.4, 5.76, 0.4, 0.55, 9.5)
+print('Минимальное значение', min(t))
+print('Максимальное значение', max(t))
+
+
+#Создать лист из 3 слов: ['Earth', 'Russia', 'Moscow'],
+#соеденить все слова в единую строку, чтобы получилось: 'Earth -> Russia -> Moscow'
+
+l1 = ['Earth', 'Russia', 'Moscow']
+print(l1[0] + l1[1] + l1[2])
